67d77e7c8ca295824eb45d806aeddd92e8d18ac7
Ken Duigraha Putra <kenduigraha@gmail.com>
2025-06-19 22:21:07 +0700

    feat: add initial form element and scripts to handle create and read data contact list with seed labels data and contact.

---
+++ assets/scripts/index.js
@@ -0,0 +1,90 @@
+function renderLabelOptions(selectedLabels = []) {
+  const select = document.getElementById('contact-label');
+  if (!select) return;
+
+  // Kosongkan dulu
+  select.innerHTML = '';
+
+  // Tambahkan setiap label sebagai <option>
+  labels.forEach(label => {
+    const option = document.createElement('option');
+    option.value = label;
+    option.textContent = label;
+
+    // Tandai sebagai selected jika sudah dipilih
+    if (selectedLabels.includes(label)) {
+      option.selected = true;
+    }
+
+    select.appendChild(option);
+  });
+}
+
+function renderContacts(filter = '') {
+  let contactList = document.getElementById('contact-list');
+  contactList.innerHTML = '';
+  console.log('contacts:', contacts);
+  // to filter contact for search
+  const filteredContacts = contacts.filter(contact => {
+    const values = Object.values(contact).join(' ').toLowerCase();
+    return values.includes(filter.toLowerCase());
+  });
+  filteredContacts
+    .forEach(contact => {
+      const li = document.createElement('li');
+      li.innerHTML = `
+        <strong>${contact.name}</strong><br>
+        ${contact.phone} - ${contact.email}<br>
+        ${contact.company} - ${contact.jobTitle}<br>
+        ${contact.address}<br>
+        <small>${contact.notes}</small><br>
+        <button onclick="editContact('${contact.id}')">Edit</button>
+        <button onclick="deleteContact('${contact.id}')">Hapus</button>
+      `;
+      contactList.appendChild(li);
+    });
+}
+
+function deleteContact(id) {
+  contacts = contacts.filter(c => c.id !== id);
+  localStorage.setItem('contacts', JSON.stringify(contacts));
+  renderContacts(searchInput.value);
+}
+
+const form = document.getElementById('contact-form');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const id = `uuid-${Date.now()}`;
+  const name = document.getElementById('name').value.trim();
+  const phone = document.getElementById('phone').value.trim();
+  const email = document.getElementById('email').value.trim();
+  const address = document.getElementById('address').value.trim();
+  const company = document.getElementById('company').value.trim();
+  const jobTitle = document.getElementById('jobTitle').value.trim();
+  const notes = document.getElementById('notes').value.trim();
+  const label = document.getElementById('contact-label').value.trim();
+
+  const existingIndex = contacts.findIndex(c => c.id === id);
+
+  const newContact = { id, name, phone, email, address, company, jobTitle, notes, label };
+  console.log('newContact: ', newContact)
+
+  // for update data
+  if (existingIndex >= 0) {
+    contacts[existingIndex] = newContact;
+  } else {
+    // create new data
+    contacts.push(newContact);
+  }
+
+  localStorage.setItem('contacts', JSON.stringify(contacts));
+  form.reset();
+  document.getElementById('contact-id').value = '';
+  renderContacts();
+});
+
+
+renderLabelOptions([]);
+renderContacts();
---
+++ assets/scripts/storage.js
@@ -0,0 +1,27 @@
+localStorage.removeItem('labels');
+localStorage.removeItem('contacts');
+
+let labels = JSON.parse(localStorage.getItem('labels')) || [];
+let contacts = JSON.parse(localStorage.getItem('contacts')) || [];
+
+// seed data
+if (labels.length === 0) {
+  labels = ['Family', 'Friends', 'Work', 'Personal'];
+  localStorage.setItem('labels', JSON.stringify(labels));
+}
+
+// seed data contacts
+if (contacts.length === 0) {
+  contacts = [{
+    id: "uuid", // unique ID
+    name: "[SEED] Nama Lengkap",
+    phone: "08123456789",
+    email: "nama@email.com",
+    address: "Alamat rumah/kantor",
+    notes: "Catatan tambahan",
+    company: "Perusahaan",
+    jobTitle: "Jabatan",
+    label: "Family"
+  }];
+  localStorage.setItem('contacts', JSON.stringify(contacts));
+}
